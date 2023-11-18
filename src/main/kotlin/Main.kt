99caaf4ad6fd14ab895439f2b413249af1862c94
Qais <Qaiscastor@gmail.com>
2023-11-18 10:30:51 +0300

    Задание 2.5

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(){
+    val n:Double = readln().toDouble()
+    val x:Double = readln().toDouble()
+
+    val y = log(n, x)
+    if (!y.isNaN()){
+        println(y)
+    }
+    else{
+        println("Целочисленный показатель не существет!")
+    }
+}
